a6ad90b7560c4b6bd7515e587761d07e158ca69a
Joylove88 <axiangjustin@gmail.com>
2022-05-24 14:42:40 +0800

    add 贸易平衡算法

--- gm-api/src/main/java/com/gm/controller/ApiTestController.java
+++ gm-api/src/main/java/com/gm/controller/ApiTestController.java
@@ -11,48 +11,16 @@ package com.gm.controller;
 import com.gm.annotation.LoginUser;
 import com.gm.common.utils.R;
 import com.gm.annotation.Login;
-import com.gm.common.web3Utils.TransactionVerifyUtils;
 import com.gm.modules.user.entity.UserEntity;
-import io.lettuce.core.output.StreamingOutput;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.web3j.abi.EventEncoder;
-import org.web3j.abi.EventValues;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Event;
-import org.web3j.abi.datatypes.Uint;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.abi.datatypes.generated.Uint8;
-import org.web3j.crypto.Credentials;
-import org.web3j.ens.contracts.generated.ENS;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.DefaultBlockParameterNumber;
-import org.web3j.protocol.core.methods.request.EthFilter;
-import org.web3j.protocol.core.methods.response.EthBlock;
-import org.web3j.protocol.core.methods.response.Log;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.exceptions.TransactionException;
-import org.web3j.tx.ClientTransactionManager;
-import org.web3j.tx.Contract;
-import org.web3j.tx.TransactionManager;
-import org.web3j.utils.Convert;
 import springfox.documentation.annotations.ApiIgnore;
-import io.reactivex.disposables.Disposable;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.*;
-import java.util.concurrent.Future;
 
 
 /**
@@ -87,4 +55,156 @@ public class ApiTestController {
 
 
 
+
+    // 1个矿工可以孵化的鸡蛋(代币)数量 （1天有86400秒，被放大了10倍）
+    static double EGGS_TO_HATCH_1MINERS = 864000;
+    static double PSN = 10000;
+    static double PSNH = 5000;
+    // 用户矿工数量
+    static Map<String,Object> miners = new HashMap<>();
+    // 用户鸡蛋数量
+    static Map<String,Object> claimedEggs = new HashMap<>();
+    // 用户上次购买矿工的时间
+    static Map<String,Object> lastHatch = new HashMap<>();
+    // 用户的推荐人
+    String referrals;
+    // 初始市场总鸡蛋数量
+    static double marketEggs = 864000000l;
+
+    static double time = System.currentTimeMillis()/1000;
+
+    static Map<String,Object> userpower =  new HashMap<>(); //用户战力
+    static double ownerBalance = 10000; // 主资金池余额
+    static double[] maplevel = {0.05,0.1,0.15,0.3,0.4};
+    static String[] userids = {"a0","a1","a2"};
+    static double totalPower = 19000;
+
+
+    public static double calculateCombatPower(String userid) {
+        return (ownerBalance / totalPower) * Double.parseDouble(String.valueOf(userpower.get(userid))) * maplevel[0] / 60;
+    }
+
+    public static void main(String[] args) {
+    for(int j = 0 ; j < 100000; j++){
+        for (int i = 0 ; i < 6; i++){
+            //购买
+            time = System.currentTimeMillis()/1000;
+            miners.put("a0",0);
+            miners.put("a1",0);
+            miners.put("a2",0);
+            miners.put("a3",0);
+            miners.put("a4",0);
+            miners.put("a5",0);
+            lastHatch.put("a0",time);
+            lastHatch.put("a1",time);
+            lastHatch.put("a2",time);
+            lastHatch.put("a3",time);
+            lastHatch.put("a4",time);
+            lastHatch.put("a5",time);
+            claimedEggs.put("a0",0);
+            claimedEggs.put("a1",0);
+            claimedEggs.put("a2",0);
+            claimedEggs.put("a3",0);
+            claimedEggs.put("a4",0);
+            claimedEggs.put("a5",0);
+
+            userpower.put("a0",1000);
+            userpower.put("a1",1500);
+            userpower.put("a2",5000);
+            userpower.put("a3",2000);
+            userpower.put("a4",6500);
+            userpower.put("a5",3000);
+            System.out.println("========================================");
+            buyEggs("a"+i);
+            System.out.println("marketEggs:" + marketEggs);
+
+            System.out.println("miners:" + miners.get("a"+i));
+            System.out.println("lastHatch:" + lastHatch.get("a"+i));
+            sellEggs("a"+i);
+        }
+    }
+        System.out.println("ownerBalance:"+ownerBalance);
+
+
+    }
+
+    // 贸易平衡算法
+    public static double calculateTrade(double rt,double rs, double bs){
+        return (PSN*bs)/(PSNH+((PSN*rs+PSNH*rt)/rt));
+    }
+
+    /// @dev 奖励复投
+    public static void hatchEggs(String userid) {
+        if(userid == userid) {
+//            ref = address(0);
+        }
+//        if(referrals == address(0) && referrals!=userid){
+//            referrals[msg.sender] = ref;
+//        }
+        double eggsUsed = getMyEggs(userid);
+        double newMiners= eggsUsed / EGGS_TO_HATCH_1MINERS;
+        miners.put(userid,Double.parseDouble(String.valueOf(miners.get(userid))) + newMiners);
+        System.out.println("claimedEggs:" + claimedEggs.get(userid));
+//        claimedEggs.put(userid,0);
+        lastHatch.put(userid, time);
+
+        // 奖励的1/10分给推荐人
+//        claimedEggs[referrals[msg.sender]] = claimedEggs[referrals[msg.sender]] + eggsUsed / 10;
+
+        // 消弱矿工囤积
+        marketEggs = marketEggs + eggsUsed / 5;
+    }
+    /// @dev 取出奖励
+    public static void sellEggs(String userid) {
+        double hasEggs = getMyEggs(userid); // 8640000000
+        double eggValue = calculateEggSell(hasEggs);
+        double fee = devFee(eggValue);
+        ownerBalance = ownerBalance - fee;
+        System.out.println("fee:"+fee);
+        System.out.println("eggValue:"+eggValue);
+        claimedEggs.put(userid,0);
+        lastHatch.put(userid, time);
+        marketEggs = marketEggs + hasEggs;
+    }
+
+    // eggs = getMyEggs
+    // sun = calculateEggSell(eggs)
+    // fee = devFee(sun)
+    // 可以提取的奖励=sun - fee
+    public static double calculateEggSell(double eggs) {
+        return calculateTrade(eggs,marketEggs,ownerBalance);
+    }
+
+    /// @dev 购买矿工
+    public static void buyEggs(String userid) {
+        System.out.println("CombatPower:"+calculateCombatPower(userid));
+        double eggsBought = calculateEggBuy(calculateCombatPower(userid), ownerBalance - calculateCombatPower(userid));
+        eggsBought = eggsBought - devFee(eggsBought);
+        double fee = devFee(calculateCombatPower(userid));
+        System.out.println("buyFee:"+fee);
+        claimedEggs.put(userid,Double.parseDouble(String.valueOf(claimedEggs.get(userid))) + eggsBought);
+        hatchEggs(userid);
+    }
+
+    public static double calculateEggBuy(double eth, double contractBalance) {
+        return calculateTrade(eth,contractBalance,marketEggs);
+    }
+    // 购买矿工兑换数量比例
+    public static double calculateEggBuySimple(long eth) {
+        return calculateEggBuy(eth,ownerBalance);
+    }
+    public static double devFee(double amount) {
+        return amount * 10 / 100;
+    }
+
+    public static double getMyEggs(String userid) {
+        return Double.parseDouble(String.valueOf(claimedEggs.get(userid))) + getEggsSinceLastHatch(userid);
+    }
+    public static double getEggsSinceLastHatch(String userid) {
+        double secondsPassed = min(EGGS_TO_HATCH_1MINERS, time - Double.parseDouble(String.valueOf(lastHatch.get(userid))));
+        return secondsPassed * Double.parseDouble(String.valueOf(miners.get(userid)));
+    }
+    public static double min(double a, double b) {
+        return a < b ? a : b;
+    }
 }
